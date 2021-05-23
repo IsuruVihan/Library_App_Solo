5126fb8c190d33ec848da19fe4ef4c1b468f091f
IsuruVihan <isuruvihan@gmail.com>
2021-05-23 18:34:33 +0530

    Image credits done

--- src/views/Library.tsx
+++ src/views/Library.tsx
@@ -13,17 +13,29 @@ const Library: FC = () => {
                 <Col className="px-0" xl={12}>
                     <img className="image" src={LibraryImage} alt="My Library" />
                 </Col>
-                <Col xl={12}>
+            </Row>
+            <Row>
+                {/*<Col />*/}
+                <Col
+                    className="image-credits px-xl-4 px-md-0 px-sm-0 py-1"
+                    xl={{offset: 9}}
+                    lg={{offset: 9}}
+                    md={{offset: 9}}
+                    sm={{offset: 9}}
+                    xs={{offset: 8}}
+                >
                     Photo by
                     <a
                         href="https://unsplash.com/@annahunko?
                         utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                        target="_blank" rel="noreferrer"
                     >
                         Anna Hunko
                     </a>
                     on
                     <a
                         href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                        target="_blank" rel="noreferrer"
                     >
                         Unsplash
                     </a>
