9e4612955d4b8a61abf6913d157770d9a82bf87d
IsuruVihan <isuruvihan@gmail.com>
2021-06-02 15:53:01 +0530

    Routing added

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,28 @@ import Footer from "./components/footer/Footer";
 import AboutUs from "./views/AboutUs";
 import ContactUs from "./views/ContactUs";
 import OriginalNavbar from "./components/navbar/OriginalNavbar";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 
 const App: FC = () => {
     return (
-        <Container className="px-0" fluid>
-            <Row className="mx-0">
-                <OriginalNavbar />
-            </Row>
-            <Row className="mx-0">
-                <Library/>
-                {/*<AboutUs />*/}
-                {/*<ContactUs />*/}
-            </Row>
-            <Row className="mx-0">
-                <Footer />
-            </Row>
-        </Container>
+        <Router>
+            <Container className="px-0" fluid>
+                <Row className="mx-0">
+                    <OriginalNavbar />
+                </Row>
+                <Row className="mx-0">
+                    <Switch>
+                        <Route path="/" exact component={Library} />
+                        <Route path="/about" component={AboutUs} />
+                        <Route path="/contactus" component={ContactUs} />
+                    </Switch>
+                </Row>
+                <Row className="mx-0">
+                    <Footer />
+                </Row>
+            </Container>
+        </Router>
     );
 }
 
