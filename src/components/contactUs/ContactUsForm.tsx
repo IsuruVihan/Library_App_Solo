6d05afed7d4320d78f8df1a372a73a56cc479f2d
IsuruVihan <isuruvihan@gmail.com>
2021-06-03 14:19:12 +0530

    Setup ContactUs styles

--- src/components/contactUs/ContactUsForm.tsx
+++ src/components/contactUs/ContactUsForm.tsx
@@ -1,51 +1,52 @@
 import React, {FC} from 'react';
 import {Button, Col, Form, Row} from "react-bootstrap";
+import "../../assets/styles/partials/_ContactUs.scss";
 
 const ContactUsForm: FC = () => {
   return (
-    <Row className="mx-0">
-      <Col xs={12} className="p-5">
+    <Row className="m-5">
+      <Col xs={12}>
         <Form>
           <Form.Row>
             <Form.Group as={Col}>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email"/>
+              <Form.Label className="label">Email</Form.Label>
+              <Form.Control className="input" type="email" placeholder="Enter email"/>
             </Form.Group>
             <Form.Group as={Col}>
-              <Form.Label>Full Name</Form.Label>
-              <Form.Control type="text" placeholder="Full name"/>
+              <Form.Label className="label">Full Name</Form.Label>
+              <Form.Control className="input" type="text" placeholder="Full name"/>
             </Form.Group>
           </Form.Row>
           <Form.Group>
-            <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="Address"/>
+            <Form.Label className="label">Address</Form.Label>
+            <Form.Control className="input" placeholder="Address"/>
           </Form.Group>
           <Form.Group>
-            <Form.Label>Address 2</Form.Label>
-            <Form.Control placeholder="Address 2"/>
+            <Form.Label className="label">Address 2</Form.Label>
+            <Form.Control className="input" placeholder="Address 2"/>
           </Form.Group>
           <Form.Row>
             <Form.Group>
-              <Form.Label>City</Form.Label>
-              <Form.Control/>
+              <Form.Label className="label">City</Form.Label>
+              <Form.Control className="input"/>
             </Form.Group>
             <Form.Group as={Col}>
-              <Form.Label>State</Form.Label>
-              <Form.Control as="select" defaultValue="Choose...">
+              <Form.Label className="label">State</Form.Label>
+              <Form.Control className="input" as="select" defaultValue="Choose...">
                 <option value="sl">Sri Lanka</option>
                 <option value="in">India</option>
                 <option value="aus">Australia</option>
               </Form.Control>
             </Form.Group>
             <Form.Group as={Col}>
-              <Form.Label>Zip</Form.Label>
-              <Form.Control/>
+              <Form.Label className="label">Zip</Form.Label>
+              <Form.Control className="input"/>
             </Form.Group>
           </Form.Row>
           <Form.Group>
             <Form.Check type="checkbox" label="Subscribe for the daily updates"/>
           </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button className="submit-btn" variant="primary" type="submit">
             Submit
           </Button>
         </Form>
